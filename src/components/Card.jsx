0ee88fead0ccfe27d95198f5a233a2362f072271
khalifa boulbayem <khalo_2@hotmail.com>
2024-03-13 15:21:03 +0000

    home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/Card.css';
+import "../styles/Card.css";
+import RatingStars from "./RatingStars";
 
-
-function Card({ title, img, description, link }) {
+function Card({ hotel }) {
+  const randomRoomIndex = Math.floor(Math.random() * hotel.rooms.length);
+  const randomFotosIndex = Math.floor(Math.random() * hotel.fotos.length);
+  const randomRoom = hotel.rooms[randomRoomIndex];
+  const randomFoto = hotel.fotos[randomFotosIndex];
   return (
     <div className="col">
-    <div className="card">
-      <img src={img} className="card-img-top" alt={title}/>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <Link to={link} className="icon-link gap-1 icon-link-hover stretched-link">
-          </Link>
-        <p className="card-text">{description}</p>
+      <div className="card">
+        <img src={randomFoto} className="card-img-top" alt={hotel.name} />
+        <div className="card-body text-start">
+          <div className="row">
+            <div className="col-8">
+              <h5 className="card-title">{hotel.name}</h5>
+              <p className="card-text">{hotel.description}</p>
+
+              <p>Price: {randomRoom.price.toFixed(2)} €</p>
+
+              <a
+                href={`hotels/${hotel.city}/${randomRoom.id}`}
+                className="stretched-link"
+              ></a>
+            </div>
+            <div className="col-4 text-end">
+              <RatingStars rating={hotel.rating}/>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
